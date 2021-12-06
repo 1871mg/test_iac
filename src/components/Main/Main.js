546f0f63efb07be55b9b8b0cc3eb80e7bf408c80
Muxaun <1871mg@gmail.com>
2021-12-06 11:48:38 +0300

    7

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -44,15 +44,28 @@ function Main() {
 						</div>
 					</div>
 
-					<div className={styles.yellowbutton}>
-						<div className={styles.yellowbuttontext}>Реализовать инициативу</div>
+					<div className={styles.ipad}>
+						<div className={styles.yellowbutton}>
+							<div className={styles.yellowbuttontext}>Реализовать инициативу</div>
+						</div>
 					</div>
 
 					<div className={styles.maincontainerright}>
 
+					<div className={styles.yellowbutton}>
+						<div className={styles.yellowbuttontext}>Реализовать инициативу</div>
+					</div>
+
+					<div className={styles.iphone}>
+						<div className={styles.yellowbutton}>
+							<div className={styles.yellowbuttontext}>Реализовать инициативу</div>
+				  	</div>
+					</div>
 
 
-						<div className={styles.left768}>
+
+
+						{/*<div className={styles.left768}>*/}
 							<div className={styles.maintexth5}>Голосовать</div>
 							<div className={styles.cyanpinkbuttons}>
 								<div className={styles.cyanbutton}>
@@ -68,16 +81,16 @@ function Main() {
 							<div className={styles.maintextvote}>За: <span className={styles.maintextvotespan}>11 789</span></div>
 							<div className={styles.maintextvote}>Против: <span className={styles.maintextvotespan}>23 213</span></div>
 							<div className={styles.maintextvote}>Всего: <span className={styles.maintextvotespan}>35 002</span></div>
-						</div>
+						{/*</div>*/}
 
-						<div className={styles.right768}>
+						{/*<div className={styles.right768}>*/}
 							<div className={styles.maintexth5}>Поделиться</div>
 							<div className={styles.socials}>
 								<div className={styles.social}><img className={styles.socialimg} src = {fb} alt="" /></div>
 								<div className={styles.social}><img className={styles.socialimg} src = {vk} alt="" /></div>
 								<div className={styles.social}><img className={styles.socialimg} src = {tw} alt="" /></div>
 								<div className={styles.social}><img className={styles.socialimg} src = {od} alt="" /></div>
-							</div>
+							{/*</div>*/}
 
 							<div className={styles.maintexth5plus23pxbottom}>Сроки</div>
 							<div className={styles.maintextvote}>До конца голосования осталось: <span className={styles.maintextvotetimespan}>41 день</span></div>
